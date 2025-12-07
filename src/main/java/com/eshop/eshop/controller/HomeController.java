0b10d29b299d3bca7e0b3eac08d00775ab1e9504
rajendrakoli91 <63579548+rajendrakoli91@users.noreply.github.com>
2025-12-07 21:45:12 +0530

    Update HomeController.java

--- src/main/java/com/eshop/eshop/controller/HomeController.java
+++ src/main/java/com/eshop/eshop/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
+ //TODO 3 add swagger/ api-doc to access all apis
     @GetMapping("/")
     public String getMethod() {
 
